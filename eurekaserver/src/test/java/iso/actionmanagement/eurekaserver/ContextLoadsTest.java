021f8f6272cfa459b4fdf5390eccad08202b3f1c
thantsoun <thantsoun@gmail.com>
2025-11-04 20:54:49 +0100

    Dummy

---
+++ eurekaserver/src/test/java/iso/actionmanagement/eurekaserver/ContextLoadsTest.java
@@ -0,0 +1,11 @@
+package iso.actionmanagement.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextLoadsTest {
+
+    @Test
+    void contextLoads() {}
+}
