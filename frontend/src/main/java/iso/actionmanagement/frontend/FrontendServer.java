17746bfba0fb4489049de724d54d3a5dee7a8434
thantsoun <thantsoun@gmail.com>
2025-11-04 21:17:02 +0100

    Dummy

---
+++ frontend/src/main/java/iso/actionmanagement/frontend/FrontendServer.java
@@ -0,0 +1,13 @@
+package iso.actionmanagement.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendServer.class, args);
+	}
+
+}
